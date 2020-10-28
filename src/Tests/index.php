5ac0504691d52bf16c927407b3e9478fda13c78f
MMDM <mmdehghan95@gmail.com>
2020-10-28 19:44:53 +0330

    get some test and fix errors

--- src/Tests/index.php
+++ src/Tests/index.php
@@ -30,22 +30,22 @@ try {
 //    $item2 = '2345';
 //    $item3 = '123';
 
-    $cart = new \Sim\Cart\Cart($pdo, $cookieStorage);
+    // fake user id
+//    $userId = 1;
 
+    // with pdo and without user
+//    $cart = new \Sim\Cart\Cart($pdo, $cookieStorage);
+
+    // with pdo and user
+//    $cart = new \Sim\Cart\Cart($pdo, $cookieStorage, $userId);
+
+    // create database tables structure
 //    $cart->utils()->runConfig();
 
 //    $cart->add($item1);
 //    $cart->add($item2);
 //    $cart->add($item3);
 //    var_dump($cart->getItems());
-
-//    var_dump($cart->totalPrice());
-//    var_dump($cart->totalDiscountedPrice());
-//    var_dump($cart->discountedPercentage($item1));
-//    var_dump($cart->discountedPercentage($item2));
-//    var_dump($cart->discountedPercentage($item3));
-//    var_dump($cart->totalDiscountedPercentage());
-//    var_dump($cart->totalDiscountedPercentage(2, true));
 
 //    $cart->update($item1, [
 //        'qnt' => 4
@@ -59,20 +59,33 @@ try {
 //    $cart->restore();
 //    var_dump($cart->getItems());
 
+//    var_dump('total price: ' . $cart->totalPrice());
+//    var_dump('total price with tax: ' . $cart->totalPriceWithTax());
+//    var_dump('total discount price: ' . $cart->totalDiscountedPrice());
+//    var_dump('total discount price with tax: ' . $cart->totalDiscountedPriceWithTax());
+//    var_dump('discount percentage item1: ' . $cart->discountedPercentage($item1));
+//    var_dump('discount percentage item2: ' . $cart->discountedPercentage($item2));
+//    var_dump('discount percentage item3: ' . $cart->discountedPercentage($item3));
+//    var_dump('total discount percentage: ' . $cart->totalDiscountedPercentage());
+//    var_dump('total discount percentage with tax: ' . $cart->totalDiscountedPercentageWithTax());
+//    var_dump('total rounded discount percentage: ' . $cart->totalDiscountedPercentage(2, true));
+//    var_dump('total rounded discount percentage with tax: ' . $cart->totalDiscountedPercentageWithTax(2, true));
+
     // test destroy items for cookie storage
 //    $cart->destroy();
 //    var_dump($cart->getItems());
 
-//    $cart->utils()->save($cart, 1);
-//    $cart->setCartName('second');
-//    $cart->utils()->save($cart, 1);
+//    $cart->utils()->save();
+//    $cart->setCartName('second')->remove($item2);
+//    $cart->utils()->save();
 //    $cart->setCartName('third');
-//    $cart->utils()->save($cart, 1, 1);
+//    $cart->utils()->save(1);
 //    $cart->setCartName('forth');
-//    $cart->utils()->save($cart, 1, 1);
+//    $cart->utils()->save(1);
 
 //    $cart->setCartName('default');
-//    $cart->utils()->fetch($cart, 1);
+//    $cart->setCartName('second');
+//    $cart->utils()->fetch();
 //    var_dump($cart->getItems());
 } catch (\Sim\Cart\Exceptions\CartMaxCountException $e) {
     echo $e->getMessage();
